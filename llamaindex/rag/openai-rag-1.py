2ce9e654e3a8f1615ae1a58003663b6a4d6c95c2
taosy <shaoyu.tao@intel.com>
2024-09-19 10:52:52 +0800

    add openai rag

---
+++ llamaindex/rag/openai-rag-1.py
@@ -0,0 +1,32 @@
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+from llama_index.core.agent import ReActAgent
+from llama_index.llms.openai import OpenAI
+from llama_index.core.tools import FunctionTool
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, Settings
+from llama_index.core.tools import QueryEngineTool
+import getpass
+import os
+
+os.environ["OPENAI_API_KEY"] = getpass.getpass()
+Settings.llm = OpenAI(model="gpt-3.5-turbo",temperature=0)
+def multiply(a: float, b: float) -> float:
+    """Multiply two numbers and returns the product"""
+    return a * b
+
+multiply_tool = FunctionTool.from_defaults(fn=multiply)
+
+def add(a: float, b: float) -> float:
+    """Add two numbers and returns the sum"""
+    return a + b
+
+add_tool = FunctionTool.from_defaults(fn=add)
+documents = SimpleDirectoryReader("../../data").load_data()
+index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+response = query_engine.query("What did the author do growing up?")
+print(response)
